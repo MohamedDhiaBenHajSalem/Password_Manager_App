aa7a2e8217b3c90e8122426e0c495f19d01a92e0
mohamed <dhia.hsalem@gmail.com>
2024-06-09 14:06:58 +0200

    json format added to save the password + handling exceptions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 import random
 import pyperclip
+import json
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -32,6 +33,13 @@ def save_command():
     web=Website_Entry.get()
     email=Email_Entry.get()
     password=password_Entry.get()
+    new_data= {
+        web:{
+            "email": str(email),
+            "password": str(password),
+
+        }
+    }
 
     if len(web)==0 or len(password)==0:
         messagebox.showinfo(title="oops",message="Please don't leave any fields empty")
@@ -39,11 +47,27 @@ def save_command():
 
         is_ok=messagebox.askokcancel(title=f"{web}",message=f"these are the details you've entered : \nEmail:{email}\nPasseword:{password}\nis it ok to save it ?")
         if is_ok:
-            with open("Password.txt","a") as record:
-                record.write(f" {web} | {email} | {password}\n")
-            Website_Entry.delete(0,END)
-            Email_Entry.delete(0,END)
-            password_Entry.delete(0,END)
+
+            try:
+
+                with open ("Password.json","r") as record:
+                    data2= json.load(record)
+
+
+            except FileNotFoundError:
+
+                with open ("Password.json","w") as record :
+                    json.dump(new_data,record,indent=4)
+
+            else :
+                data2.update(new_data)
+                with open("Password.json","w") as record:
+                    json.dump(data2,record,indent=4)
+            finally :
+
+                    Website_Entry.delete(0,END)
+                    Email_Entry.delete(0,END)
+                    password_Entry.delete(0,END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 from tkinter import *
